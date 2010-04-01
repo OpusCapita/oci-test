758c5ecf8901b2b3ed2a2430a6c3eb7e0e94175a
shulga <devnull@localhost>
2010-04-01 15:54:32 +0000

    (OPC-3625) Refactored to use encryption logic from OCI library

--- src/java/com/jcatalog/ocisetest/security/IbmOciRequestEncrypter.java
+++ src/java/com/jcatalog/ocisetest/security/IbmOciRequestEncrypter.java
@@ -1,16 +1,14 @@
 package com.jcatalog.ocisetest.security;
 
+import com.jcatalog.oci.section.outbound.encryptor.Encryptor;
+
+import org.apache.commons.lang.StringUtils;
+
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.lang.StringUtils;
 
 
 /**
@@ -22,14 +20,14 @@ public class IbmOciRequestEncrypter implements OciRequestEncrypter {
     private static final String PASSWORD_PARAM = "password";
     private static final String VALIDITY_INTERVAL_PARAM = "validityInterval";
     private static final String SECRET_KEY_PARAM = "secretKey";
-    private static final String ALGORITHM_TYPE = "DES";
-    private Base64 base64 = new Base64();
     private List standardParams;
 
     public Map encrypt(Map params) throws Exception {
         Map encryptedParams = new HashMap();
 
         String secretKey = (String) params.get(SECRET_KEY_PARAM);
+
+        Encryptor encryptor = new Encryptor(secretKey);
 
         for (Iterator iterator = params.entrySet().iterator(); iterator.hasNext();) {
             Map.Entry param = (Map.Entry) iterator.next();
@@ -42,8 +40,8 @@ public class IbmOciRequestEncrypter implements OciRequestEncrypter {
                 encryptedParams.put(name, value);
             } else {
                 if (PASSWORD_PARAM.equals(name)) {
-
                     String validityIntervalString = (String) params.get(VALIDITY_INTERVAL_PARAM);
+
                     // setting valid interval to password
                     int validityInterval = StringUtils.isNotBlank(validityIntervalString)
                         ? Integer.parseInt(validityIntervalString) : 0;
@@ -53,21 +51,10 @@ public class IbmOciRequestEncrypter implements OciRequestEncrypter {
                     value = value + " " + validTo.getTime();
                 }
 
-                encryptedParams.put(name, encrypt(secretKey, value));
+                encryptedParams.put(name, encryptor.encrypt(value));
             }
         }
         return encryptedParams;
-    }
-
-    private String encrypt(String keyString, String inputString) throws Exception {
-        byte[] key = keyString.getBytes();
-        SecretKeySpec sks = new SecretKeySpec(key, ALGORITHM_TYPE);
-        Cipher algorithm = Cipher.getInstance(ALGORITHM_TYPE);
-        algorithm.init(Cipher.ENCRYPT_MODE, sks);
-
-        byte[] encrypted = algorithm.doFinal(inputString.getBytes());
-
-        return new String(base64.encode(encrypted));
     }
 
     public void setStandardParams(List standardParams) {
